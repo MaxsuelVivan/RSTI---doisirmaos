ce7526a6d116b78093d4e39f0586e736e88cdc3d
MaxsuelVivan <maxsuelvivan668@gmail.com>
2024-02-16 16:49:09 -0300

    aula sobre arrays e funcoes

---
+++ index.js
@@ -0,0 +1,80 @@
+1. // silence is golden
+/*
+var nome = prompt("Escreva seu nome");
+var cor = prompt ("Escreva sua cor favorita");
+
+var frase = "A cor favorita de " + nome + " é " + cor + " . ";
+
+console.log(frase);*/
+
+
+2./*var frase2 = prompt("Escreva uma frase");
+console.log(frase2.toULocaleUpperCase());
+console.log(frase2.replaceAll9("o","i"));
+console.log(frase2length);*/
+
+/*const listaDeCompras = ["Abacate", "Banana", "Tomate"]
+const segundoItem = listaDeCompras [2] // "Tomate"*/
+
+
+3./*const listaDeCachorros = ["A","B","C","D","E"];
+var cachorro = prompt("Escolha um número de 0 a 4");
+const segundoItem = listaDeCachorros[cachorro];
+
+console.log(cachorro)[escolha];*/
+
+
+4./*var array= ["a", "b", "c", "d", "e", "f", "g", "h"];
+//         [ 0,   1,   2,  3,  4,  5,  6,  7,      ];
+array.splice(3, 3)
+console.log(array);*/
+
+
+5./*var array = [1,2,3,4,5,6];
+    array.log(array.length);
+    console.push(7);
+    array.splicr(3, 2);
+    console.log(array.lenght);*/
+
+
+6./*const altura = 2
+const largura = 3
+const area = altura * largura
+console.log(area)*/
+
+7./*function calculaArea(altura, largura) {
+    const area = altura * largura
+    console.log(area)
+}
+
+calculaArea(2, 3);*/
+
+ 
+8./*function imprimirOlaMundo() {
+    console.log("OláMundo!")
+}
+    imprimirOlaMundo();*/
+
+/*
+function funcaonome(nome){
+    
+    console.log(`Olá ${nome}`);
+    }
+
+funcaonome("nome"); // <--funcao sendo chamada
+//funcaNome("Max");
+*/
+
+    
+
+
+
+
+
+    
+
+
+
+
+
+
